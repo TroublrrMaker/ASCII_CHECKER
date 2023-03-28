ceae6fefe1a6c8ca9d964d1f9055aa84df773a57
TroublrrMaker <86102901+TroublrrMaker@users.noreply.github.com>
2023-03-28 20:41:34 +0000

    Simple Calculator

--- main.c
+++ main.c
@@ -1,27 +1,65 @@
-/** This program was built to show user the ASCII code of any integer
- *  Inspiration from a video tutorial in which the tutor said "You dont need to memorise all the ASCII code"
-  */
 #include <stdio.h>
 #include <stdlib.h>
-// Main is the entry point of the program
+
 int main()
 {
- // Declaring the variables to be used
-    int num;
-     char Ascii;
-     // Adding title to the program
-     printf(“\033[0;31m”); //Set the text to the color red
-     printf(“\033[0m”); //Resets the text to default color
-     printf("--------------------------------------------------------\n");
-     printf("       PROGRAM TO PRINT ASCII CODE OF ANY INTEGER       \n");
-     printf("--------------------------------------------------------\n\n");
-     printf(“\033[0m”); //Resets the text to default color
-// Program starts by asking user the integer to search for
-    printf("Enter Integer: ");
-    scanf("%d", &num);
-    Ascii = num;
-    // Program outputs the corresponding Ascii code of the integer entered by user
-    printf("The ASCII code for %d is: %c", num , Ascii);
+int opt, num1, num2;
+float res;
+   printf("--------------------------------------------------\n");
+     printf("          TROUBLRRMAKER'S CALCULATOR       \n");
+     printf("--------------------------------------------------\n\n");
+printf("1. ADDITION\n");
+printf("2. SUBSTRACTION\n");
+printf("3. MULTIPLICATION\n");
+printf("4. DIVISION\n");
 
+printf("Select Your Operation (1 - 4): ");
+scanf("%d", &opt);
+
+if (opt == 1)
+{
+    printf("YOU HAVE CHOSEN ADDITION\n");
+     printf("Enter Number 1: ");
+     scanf("%d", &num1);
+     printf("%d + ", num1);
+     scanf("%d", &num2);
+     res = num1 + num2;
+     printf("%d + %d = %.1f", num1, num2, res);
+}
+else if (opt == 2)
+{
+    printf("YOU HAVE CHOSEN SUBSTRACTION\n");
+     printf("Enter Number 1: ");
+     scanf("%d", &num1);
+     printf("%d - ", num1);
+     scanf("%d", &num2);
+     res = num1 - num2;
+     printf("%d - %d = %.1f", num1, num2, res);
+}
+else if (opt == 3)
+{
+    printf("YOU HAVE CHOSEN MULTIPLICATION\n");
+     printf("Enter Number 1: ");
+     scanf("%d", &num1);
+     printf("%d x ", num1);
+     scanf("%d", &num2);
+     res = num1 * num2;
+     printf("%d x %d = %.1f", num1, num2, res);
+}
+else if (opt == 4)
+{
+    printf("YOU HAVE CHOSEN DIVISION\n");
+     printf("Enter Number 1: ");
+     scanf("%d", &num1);
+     printf("%d ÷ ", num1);
+     scanf("%d", &num2);
+     res = num1 / num2;
+     printf("%d ÷ %d = %.2f", num1, num2, res);
+}
+else
+{
+    printf("YOU HAVE MADE A WRONG SELECTION\n");
+    system("pause");
+}
     return 0;
 }
